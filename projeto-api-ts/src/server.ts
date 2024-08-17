fb659d996e96733ffd9af4046530c830208fb317
Matheus Silva <matpaixaoo@outlook.com>
2024-08-16 22:18:39 -0300

    feat: implementação do CREATE da entity Task

--- projeto-api-ts/src/server.ts
+++ projeto-api-ts/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express"
 import dataBase from "./database/ormconfig"
+import routes from "./routes"
 
 const app = express()
 const port = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`Servidor executando na porta ${port}`)
